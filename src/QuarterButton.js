02e75474cb4d3519365569fc4b0ed85b56d3cc49
NomadDaniel <reachingdaniel@gmail.com>
2020-04-13 19:43:40 -0400

    safety push

--- src/QuarterButton.js
+++ src/QuarterButton.js
@@ -5,9 +5,9 @@ import "./App.css"
 function QuarterButton(props) {
 
   return (
-
+          
           <button onClick= { () =>props.quarterButton < 4 ? props.setQuarterButton(props.quarterButton + 1) : props.setQuarterButton(1)}>Change Quarter</button>
-
+          
           ) 
 }
 
